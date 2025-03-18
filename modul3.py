5eb7bef47f589b4a176203ede00371cf89a7e8c3
Adebliauliazami <163020203+Adebliauliazami@users.noreply.github.com>
2025-03-18 22:18:31 +0700

    Add files via upload

---
+++ modul3.py
@@ -0,0 +1,10 @@
+M = float(input("Masukkan Modal Awal Investasi : "))
+r = float(input("Masukkan Suku bunga tahunan (%) : "))
+T = float(input("Masukkan target investasi : "))
+
+tahun = 0
+while M < T:
+    M += M * (r / 100)
+    tahun += 1
+    print(f"Tahun ke-{tahun}: Rp{M}")
+print(f"Target tercapai dalam {tahun} tahun! ")
